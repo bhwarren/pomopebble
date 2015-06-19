816f6c861cb81f6da8adc75d5be16e4050749dee
kalangaum <lucao.rodrigues@gmail.com>
2015-06-18 22:54:47 -0300

    images

--- src/windows/mainwindow.c
+++ src/windows/mainwindow.c
@@ -5,7 +5,9 @@
 // BEGIN AUTO-GENERATED UI CODE; DO NOT MODIFY
 static Window *s_window;
 static GFont s_res_gothic_28_bold;
-static GBitmap *s_res_start_button;
+static GBitmap *s_res_play_btn;
+static GBitmap *s_res_cog_btn;
+static GBitmap *s_res_reload_btn;
 static TextLayer *s_textlayer_1;
 static ActionBarLayer *s_actionbarlayer_1;
 
@@ -16,9 +18,11 @@ static void initialise_ui(void) {
   #endif
   
   s_res_gothic_28_bold = fonts_get_system_font(FONT_KEY_GOTHIC_28_BOLD);
-  s_res_start_button = gbitmap_create_with_resource(RESOURCE_ID_START_BUTTON);
+  s_res_play_btn = gbitmap_create_with_resource(RESOURCE_ID_PLAY_BTN);
+  s_res_cog_btn = gbitmap_create_with_resource(RESOURCE_ID_COG_BTN);
+  s_res_reload_btn = gbitmap_create_with_resource(RESOURCE_ID_RELOAD_BTN);
   // s_textlayer_1
-  s_textlayer_1 = text_layer_create(GRect(18, 68, 100, 30));
+  s_textlayer_1 = text_layer_create(GRect(18, 60, 100, 30));
   text_layer_set_background_color(s_textlayer_1, GColorClear);
   text_layer_set_text(s_textlayer_1, "Lucas!!");
   text_layer_set_text_alignment(s_textlayer_1, GTextAlignmentCenter);
@@ -28,8 +32,10 @@ static void initialise_ui(void) {
   // s_actionbarlayer_1
   s_actionbarlayer_1 = action_bar_layer_create();
   action_bar_layer_add_to_window(s_actionbarlayer_1, s_window);
-  action_bar_layer_set_background_color(s_actionbarlayer_1, GColorBlack);
-  action_bar_layer_set_icon(s_actionbarlayer_1, BUTTON_ID_SELECT, s_res_start_button);
+  action_bar_layer_set_background_color(s_actionbarlayer_1, GColorWhite);
+  action_bar_layer_set_icon(s_actionbarlayer_1, BUTTON_ID_UP, s_res_cog_btn);
+  action_bar_layer_set_icon(s_actionbarlayer_1, BUTTON_ID_SELECT, s_res_play_btn);
+  action_bar_layer_set_icon(s_actionbarlayer_1, BUTTON_ID_DOWN, s_res_reload_btn);
   layer_add_child(window_get_root_layer(s_window), (Layer *)s_actionbarlayer_1);
 }
 
@@ -37,7 +43,9 @@ static void destroy_ui(void) {
   window_destroy(s_window);
   text_layer_destroy(s_textlayer_1);
   action_bar_layer_destroy(s_actionbarlayer_1);
-  gbitmap_destroy(s_res_start_button);
+  gbitmap_destroy(s_res_play_btn);
+  gbitmap_destroy(s_res_cog_btn);
+  gbitmap_destroy(s_res_reload_btn);
 }
 // END AUTO-GENERATED UI CODE
 
